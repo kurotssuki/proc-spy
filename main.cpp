2c68436f27e8ce91391f01d4f73fc8316d2897ac
kurotssuki <lel.romaxkp@gmail.com>
2026-02-11 16:16:07 +0000

    Added extraction of process names from /proc/PID/comm

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <filesystem>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
 namespace fs = std::filesystem;
+
 
 bool isNumber(const std::string& s);
 
@@ -13,15 +15,26 @@ int main() {
         for (const auto& entry : fs::directory_iterator(directoryPath)) {
             if(entry.is_directory()) {
                 std::string folderName = entry.path().filename().string();
-            
+                                               
                 if(isNumber(folderName)) {
-                    std::cout << folderName << std::endl;
+                    std::string processInfo = (entry.path() / "comm").string();
+                    std::string str = "";
+                    std::ifstream file(processInfo);
+                    if(file.is_open()) {
+                        if(std::getline(file, str)) {
+                            std::cout << "[PID: " << folderName << "] " << str << std::endl;
+                        } else {
+                            std::cerr << "Error reading process name, empty?" << std::endl;
+                        }
+                   } 
                 }
             }
         }
-    }    
+    }
+    return 0;
 }
 
 bool isNumber(const std::string& s) {
     return !s.empty() && all_of(s.begin(), s.end(), ::isdigit);
 }
+
