6af7f9afa8ec2530c593fe980ce0630527b33e0e
kurotssuki <lel.romaxkp@gmail.com>
2026-02-12 19:26:24 +0000

    feat: resolve numeric loginuid to username in process list

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <pwd.h>
+#include <unistd.h>
 
 namespace fs = std::filesystem;
 
-
 bool isNumber(const std::string& s);
+std::string uidToUsername(const std::string& s);
+
 
 int main() {
     std::string directoryPath = "/proc/";
@@ -17,17 +20,26 @@ int main() {
                 std::string folderName = entry.path().filename().string();
                                                
                 if(isNumber(folderName)) {
-                    std::string processInfo = (entry.path() / "comm").string();
-                    std::string str = "";
-                    std::ifstream file(processInfo);
-                    if(file.is_open()) {
-                        if(std::getline(file, str)) {
-                            std::cout << "[PID: " << folderName << "] " << str << std::endl;
-                        } else {
-                            std::cerr << "Error reading process name, empty?" << std::endl;
-                        }
-                   } 
-                }
+                    std::string commPath = (entry.path() / "comm").string();
+                    std::string cmdlinePath = (entry.path() / "cmdline").string();
+                    std::string loginuidPath = (entry.path() / "loginuid").string();
+                    std::string comm, cmdline, loginuid;
+
+                    std::ifstream commFile(commPath);
+                    std::getline(commFile, comm);
+                    
+                    std::ifstream cmdlineFile(cmdlinePath);
+                    std::getline(cmdlineFile, cmdline);
+
+                    std::ifstream loginuidFile(loginuidPath);
+                    std::getline(loginuidFile, loginuid);
+                    
+//need to implement loginuid to username here...
+                    std::string username = uidToUsername(loginuid);
+
+                    std::cout << "[PID: " << folderName << "] [" << 
+                    comm << "] " << username << std::endl;  
+               }
             }
         }
     }
@@ -37,4 +49,17 @@ int main() {
 bool isNumber(const std::string& s) {
     return !s.empty() && all_of(s.begin(), s.end(), ::isdigit);
 }
-
+std::string uidToUsername(const std::string& s) {
+    try {
+        uid_t uid = std::stoul(s);
+        if(uid == (uid_t)-1) {
+            return "unset";
+        }
+        struct passwd *pw = getpwuid(uid);
+        if(pw) {
+            return std::string(pw->pw_name);
+        }
+    } catch (...) {
+        return "invalid uid";
+    }
+}
