d4258dfc9aa0560d52cf2a65e73753a752c8258b
kurotssuki <lel.romaxkp@gmail.com>
2026-02-13 16:16:58 +0000

    Refactor: introduce Process class for /proc parsing
    Moved all process-related logic (reading files, parsing UIDs) out of main.cpp
    and into a dedicated Process class to improve modularity.

--- main.cpp
+++ main.cpp
@@ -8,58 +8,83 @@
 
 namespace fs = std::filesystem;
 
-bool isNumber(const std::string& s);
-std::string uidToUsername(const std::string& s);
+class Process {
+    private:
+        std::string pid;
+        std::string name;
+        std::string username;
+        std::string cmdline;
+
+        //Helper: Convert UID to Username
+        std::string uidToUsername(const std::string& s) {
+            try {
+                uid_t uid = std::stoul(s);
+                if(uid == (uid_t)-1) return "unset";
+
+                struct passwd *pw = getpwuid(uid);
+                if(pw)  return std::string(pw->pw_name);                      
+            } catch (...) {
+                return "invalid uid";
+            }
+        }
+        //Helper: Read first line of a file;
+        std::string readFile(const std::string& path) {
+            std::ifstream file("/proc/" + pid + "/" + path);
+            std::string line;
+            if(file.is_open()) {
+                std::getline(file, line);
+            }
+            return line;
+        }
+
+
+
+
+    public:
+        //Constructor: Takes PID returns process info
+        Process(const std::string& pidStr) : pid(pidStr) {
+            refresh();
+        }
+        //Loads proc data
+        void refresh() {
+            name = readFile("comm");
+            cmdline = readFile("cmdline");
+
+            std::string uid = readFile("loginuid");
+            username = uidToUsername(uid);
+        }
+        void printInfo() const {
+            std::cout << "[PID: " << pid <<
+                 "] [name: " << name << 
+                 "] [cmdline: " << cmdline << 
+                 "] [user: " << username << ']' << std::endl;
+        }
+};
+
+
+//Utilily: checks if directory is a PID
+bool isPIDfolder(const std::string& s) {
+    return !s.empty() && all_of(s.begin(), s.end(), ::isdigit);
+}
+
 
 
 int main() {
-    std::string directoryPath = "/proc/";
-    if(fs::exists(directoryPath)) {
-        for (const auto& entry : fs::directory_iterator(directoryPath)) {
+    std::string procPath = "/proc/";
+    if(fs::exists(procPath)) {
+        for (const auto& entry : fs::directory_iterator(procPath)) {
             if(entry.is_directory()) {
                 std::string folderName = entry.path().filename().string();
-                                               
-                if(isNumber(folderName)) {
-                    std::string commPath = (entry.path() / "comm").string();
-                    std::string cmdlinePath = (entry.path() / "cmdline").string();
-                    std::string loginuidPath = (entry.path() / "loginuid").string();
-                    std::string comm, cmdline, loginuid;
 
-                    std::ifstream commFile(commPath);
-                    std::getline(commFile, comm);
-                    
-                    std::ifstream cmdlineFile(cmdlinePath);
-                    std::getline(cmdlineFile, cmdline);
-
-                    std::ifstream loginuidFile(loginuidPath);
-                    std::getline(loginuidFile, loginuid);
-                    
-//need to implement loginuid to username here...
-                    std::string username = uidToUsername(loginuid);
-
-                    std::cout << "[PID: " << folderName << "] [" << 
-                    comm << "] " << username << std::endl;  
-               }
+                if(isPIDfolder(folderName)) {
+                    Process proc(folderName);
+                    proc.printInfo();
+                }
+               
             }
         }
     }
     return 0;
 }
 
-bool isNumber(const std::string& s) {
-    return !s.empty() && all_of(s.begin(), s.end(), ::isdigit);
-}
-std::string uidToUsername(const std::string& s) {
-    try {
-        uid_t uid = std::stoul(s);
-        if(uid == (uid_t)-1) {
-            return "unset";
-        }
-        struct passwd *pw = getpwuid(uid);
-        if(pw) {
-            return std::string(pw->pw_name);
-        }
-    } catch (...) {
-        return "invalid uid";
-    }
-}
+
